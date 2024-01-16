19f3060405a2e603c06426af38754c8629ad8504
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-16 16:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function createDom(root) {
+  if (typeof root === "string") {
+    return document.createTextNode(root);
+  }
 
+  const node = document.createElement(root.type);
+
+  if (root.attributes) {
+    for (const [key, value] of Object.entries(root.attributes)) {
+      node.setAttribute(key, value);
+    }
+  }
+
+  if (root.children) {
+    for (const child of root.children) {
+      const childNode = createDom(child);
+      node.appendChild(childNode);
+    }
+  }
+
+  return node;
 }
 
-module.exports=createDom
+module.exports=createDom;
